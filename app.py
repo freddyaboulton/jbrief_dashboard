6ea915be35d8828b7fb5f43df91ba8e443eca2fb
freddyaboulton <freddyalfonsoboulton@gmail.com>
2020-03-17 23:57:31 -0400

    Using dash bootstrap components. Now plot 10 most recent games

--- app.py
+++ app.py
@@ -4,16 +4,18 @@ import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 import pandas as pd
 from dash.dependencies import Input, Output
 from flask_caching import Cache
 
-from queries import get_past_winners
+from queries import get_past_winners, get_game_trend, get_10_latest_games
+from utilities import get_unique_contestant_ids
 from constants import DATE_FORMAT
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = [dbc.themes.DARKLY]
 
-app = dash.Dash("jbrief", external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 CACHE_CONFIG = {
@@ -23,66 +25,130 @@ CACHE_CONFIG = {
 cache = Cache()
 cache.init_app(server, config=CACHE_CONFIG)
 
-@cache.memoize()
-def get_winners_df(key):
-    assert "winners_" in key, f"get_winners_df key not properly formatted {key}"
-    date = key.split("_")[1]
-    return get_past_winners(date)
+colors = {
+    'darkblue': '#061E44',
+    'jeopardy-yellow': '#FFCC00',
+    'white': '#FFFFFF',
+    'grey': '#808080'
+}
 
-app.layout = html.Div(
-    children=[
-    html.H1(children='Jbrief',
-            style={"color": '#7FDBFF',
-                   'textAlign': 'center'}),
-    html.Div(children='''
-        Jeopardy games at a glance
-    ''',
-            style={"textAlign": 'center'}),
-    html.Div([
-        html.Div([
+
+@cache.memoize()
+def get_past_game_ids_df(key: str) -> pd.DataFrame:
+    assert 'past-games_' in key, f"get_past_game_ids_df key not properly formatted {key}."
+    return get_10_latest_games()
+
+
+@cache.memoize()
+def get_winners_df(key: str) -> pd.DataFrame:
+    assert "winners_" in key, f"get_winners_df key not properly formatted {key}."
+    return get_past_winners()
+
+
+@cache.memoize()
+def get_game_trend_df(key: str) -> pd.DataFrame:
+    assert "game-trend_" in key, f'get_game_trend key not properly formatted {key}.'
+    game_id = key.split("_")[1]
+    return get_game_trend(game_id)
+
+
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col([
+            html.H1('JBrief')
+        ],
+        width={'size': 8, 'offset': 2},
+        style={'textAlign': 'center',
+               'color': colors['white']}),
+    dbc.Col([
+        html.A([
+            html.Img(src='assets/github_logo.png',
+                     style={'height': '40%', 'width': '40%',
+                            'margin': '12px',
+                            'textAlign': 'right'})
+            ],
+            href='https://github.com/freddyaboulton/jbrief_dashboard',
+            )
+        ],
+         width=1),
+    ]),
+
+    html.H4(['Jeopardy games at a glance'],
+    style={"textAlign": 'center',
+            'color': colors['white']}),
+    dbc.Row([
+        dbc.Col([
             dcc.Graph(id='latest-winners'),
             dcc.Interval(id='interval',
                          interval=1*1000,
                          n_intervals=0)
-        ], className='six columns'),
-        html.Div([
-            dcc.Graph(
-                id='example-graph-2',
-                figure={
-                    'data': [
-                        {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'Foo'},
-                        {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': 'Bar'},
-                    ],
-                    'layout': {
-                        'title': 'Plot 2',
-                        'plot_bgcolor': "#111111",
-                        'paper_bgcolor': '#111111',
-                        'font': {'color': '#7FDBFF'}
-                    }
-                }
-                )
-        ], className='six columns'),
-    ])
+            ], 
+            width={'size': 6}
+        ),
+        dbc.Col([
+            dcc.Graph(id='game-trend')
+        ], width=6),
+    ], justify='center')
 ])
+
 
 @app.callback(Output('latest-winners', 'figure'),
               [Input('interval', 'n_intervals')])
 def update_latest_winners_graph(n):
-    winners = get_winners_df(f'winners_{datetime.datetime.now().strftime(DATE_FORMAT)}')
+    todays_date = datetime.datetime.now().strftime(DATE_FORMAT)
+    winners = get_winners_df(f'winners_{todays_date}')
     data = [{'x': winners.date.tolist(),
              'y': winners.final_amount.tolist(),
-             'text': winners.graph_text.tolist()}]
+             'text': winners.graph_text.tolist(),
+             'marker': {'size': 10}
+             }]
     layout = {'title': 'Latest Winners',
               'hovermode': 'closest',
               'type': 'linear',
-              'xaxis': {'tickmode': 'array',
-                        'tickvals': pd.date_range(winners.date.min(),
-                                              winners.date.max()).tolist()
-                        },
-              'plot_bgcolor': "#111111",
-              'paper_bgcolor': '#111111',
-              'font': {'color': '#7FDBFF'}}
+               'xaxis': {'title': 'Date of Game',
+                         'gridcolor': colors['grey']},
+               'yaxis': {'title': 'Dollars Won',
+                         'gridcolor': colors['grey']},
+              'plot_bgcolor': colors['darkblue'],
+              'paper_bgcolor': colors['darkblue'],
+              'font': {'color': colors['white']}}
     return {'data': data, 'layout': layout}
+
+
+@app.callback(Output('game-trend', 'figure'),
+              [Input('latest-winners', 'clickData')])
+def update_game_trend_graph(clickData):
+    todays_date = datetime.datetime.now().strftime(DATE_FORMAT)
+    past_game_ids = get_past_game_ids_df(f'past-games_{todays_date}')
+
+    if not clickData:
+        # df sorted in descending order so get the first point for
+        # latest game
+        date = past_game_ids.date.tolist()[0]
+    else:
+        date = clickData['points'][0]['x']
+
+    game_trend = get_game_trend_df(f'game-trend_{date}')
+    contestant_ids = [int(ID) for ID in get_unique_contestant_ids(game_trend).split(",")]
+
+    data = [{'x': game_trend.clue_order_number.loc[game_trend.contestant_id == contestant_id].tolist(),
+             'y': game_trend.running_total.loc[game_trend.contestant_id == contestant_id].tolist(),
+             'name': game_trend.graph_text.loc[game_trend.contestant_id == contestant_id].tolist()[0],
+             'mode': 'lines+markers'
+             } for contestant_id in contestant_ids]
+    layout = {'title': f'Game Trend for {date}',
+              'type': 'linear',
+              'xaxis': {'title': 'Number of Clues Asked In Game',
+                        'dtick': 10,
+                        'gridcolor': colors['grey']},
+              'yaxis': {'title': 'Dollars Won In Game',
+                        'gridcolor': colors['grey']},
+              'plot_bgcolor': colors['darkblue'],
+              'paper_bgcolor': colors['darkblue'],
+              'font': {'color': colors['white']},
+             }
+    return {'data': data, 'layout': layout}
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- queries.py
+++ queries.py
@@ -1,24 +1,23 @@
 import pandas as pd
 import datetime
+from typing import List
 
 from constants import DB_CONNECTION, DATE_FORMAT
+from utilities import get_unique_contestant_ids
 
-def get_past_game_ids(date_string: str) -> pd.DataFrame:
-    today = datetime.datetime.strptime(date_string, DATE_FORMAT)
-    minus_10 = today - datetime.timedelta(days=10)
+def get_10_latest_games() -> pd.DataFrame:
     return pd.read_sql_query("""
-                            SELECT game_id,
-                                   date
-                            FROM date where 
-                            to_date(date, 'YYYY-MM-DD') >= to_date('{minus_10}', 'YYYY-MM-DD')
-                            AND to_date(date, 'YYYY-MM-DD') <= to_date('{today}', 'YYYY-MM-DD')
-                            """.format(minus_10=minus_10.strftime(DATE_FORMAT),
-                                        today=today.strftime(DATE_FORMAT)),
+                             SELECT game_id,
+                                    date
+                             FROM date
+                             ORDER BY date desc
+                             limit 10       
+                             """,
                             DB_CONNECTION)
 
 
 def get_contestant_info(contestant_ids: str) -> pd.DataFrame:
-    return pd.read_sql_query("""
+	return pd.read_sql_query("""
                              SELECT contestant_id,
                                     first_name,
                                     last_name,
@@ -30,29 +29,62 @@ def get_contestant_info(contestant_ids: str) -> pd.DataFrame:
                              DB_CONNECTION)
 
 
-def get_past_winners(date_string: str) -> pd.DataFrame:
+def get_past_winners() -> pd.DataFrame:
+	
+	past_game_ids = get_10_latest_games()
+	ids_as_string = ",".join(past_game_ids.game_id.unique().astype('str').tolist())
 
-    past_game_ids = get_past_game_ids(date_string)
-    ids_as_strings = past_game_ids.game_id.astype("str").tolist()
-    game_results = pd.read_sql_query("""
-                                     SELECT game_id,
+	game_results = pd.read_sql_query("""
+									 SELECT game_id,
                                             contestant_id,
                                             SUM(change_in_value) as final_amount
                                      FROM game
                                      WHERE game_id IN ({game_ids})
                                      GROUP BY game_id, contestant_id
-                                     """.format(game_ids=",".join(ids_as_strings)),
+                                     """.format(game_ids=ids_as_string),
                                      DB_CONNECTION)
-    winners = game_results.groupby("game_id").\
+	winners = game_results.groupby("game_id").\
                  agg(lambda df: df.loc[df['final_amount'].idxmax()]).\
                  reset_index()
-    contestants = ",".join(winners.contestant_id.astype("int").astype('str').\
-                                   unique().tolist())
-    contestant_info = get_contestant_info(contestants)
-    winners = winners.merge(past_game_ids).merge(contestant_info)
-    winners['graph_text'] = (winners['first_name'] + " " + 
+	contestants = get_unique_contestant_ids(winners)
+	contestant_info = get_contestant_info(contestants)
+	winners = winners.merge(past_game_ids).merge(contestant_info)
+	winners['graph_text'] = (winners['first_name'] + " " + 
                              winners['last_name'] + ", " + 
                              winners['hometown'] + ", " +
                              winners['occupation'])
-    return winners[['date', 'final_amount', 'graph_text']]
-    
+	return winners[['date', 'final_amount', 'graph_text']]
+
+
+def get_game_trend(date: str) -> pd.DataFrame:
+
+       game_id = pd.read_sql_query("""
+                                   SELECT game_id
+                                   FROM date
+                                   WHERE date = '{date}'
+                                   """.format(date=date),
+                                   DB_CONNECTION)
+       
+       game_id = game_id.game_id.iloc[0]
+
+       game_trend_query = """
+                          SELECT contestant_id,
+                          clue_order_number,
+                          SUM(change_in_value) 
+                          OVER (PARTITION BY contestant_id
+                                ORDER BY clue_order_number asc rows between unbounded preceding and current row)
+                          AS running_total
+                          FROM game
+                          WHERE game_id = {game_id}
+                          """
+
+
+       game_trend = pd.read_sql_query(game_trend_query.format(game_id=game_id),
+                                      DB_CONNECTION)
+
+       contestants = get_unique_contestant_ids(game_trend)
+       contestant_info = get_contestant_info(contestants)
+       game_trend = game_trend.merge(contestant_info)
+       game_trend['graph_text'] = (game_trend['first_name'] + " " + 
+                                   game_trend['last_name'])
+       return game_trend
